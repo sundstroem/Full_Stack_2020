5d8a7ec0c127f54a57997f10718a040b60244ca5
suanders <anders.sundstrom@aaalto.fi>
2020-08-23 18:26:20 +0300

    4.1

---
+++ part4/blogilista/index.js
@@ -0,0 +1,44 @@
+require('dotenv').config()
+const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+
+const url = process.env.MONGODB_URI
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
+
+const Blog = require('./models/blog')
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
